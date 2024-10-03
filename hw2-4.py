379f0781b26962c2023fd1dab9590526cdeb33c0
hussein-21 <husseinw@umich.edu>
2024-10-03 02:31:17 -0400

    Delete hw2-4.py

--- hw2-4.py
+++
@@ -1,8 +0,0 @@
-# Name: Hussein Alsaidi
-# Date: 02/08/2023
-# Description: This is part 4 of Homework #2.
-from geom_funcs import getsidelength1, getsidelength2, calcRecaArea
-side1=getsidelength1()
-side2=getsidelength2()
-calcRecaArea(side1,side2)
-
