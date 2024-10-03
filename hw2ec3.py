2e7ab846f061920765f953f0e09c5ae57ecf37f8
hussein-21 <husseinw@umich.edu>
2024-10-03 02:32:45 -0400

    Delete hw2ec3.py

--- hw2ec3.py
+++
@@ -1,7 +0,0 @@
-# Name: Hussein Alsaidi
-# Date: 02/08/2023
-# Description: This is part 3 of Homework #2 Extra Credit.
-#------------------------------------------------------------
-fahrenheit = float(input("Enter temperature in fahrenheit: "))
-celsius = (fahrenheit - 32) * 5 / 9
-print('%.1f Fahrenheit is equal to %0.2f Celsius' % (fahrenheit, celsius))
